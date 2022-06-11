953fcdfc7f49b532d2487cd7fb74d483c1153785
Ivan Maljukanović <imoptimal@gmail.com>
2022-06-11 19:17:58 +0200

    Update cookies-consent.js

--- wp-content/themes/twentytwentytwo-child/cookies-consent.js
+++ wp-content/themes/twentytwentytwo-child/cookies-consent.js
@@ -1,3 +1,6 @@
 document.addEventListener('DOMContentLoaded', function() {
-    cookieconsent.run({ "notice_banner_type": "headline", "consent_type": "simple", "palette": "dark", "language": "en", "page_load_consent_levels": ["strictly-necessary"], "notice_banner_reject_button_hide": false, "preferences_center_close_button_hide": false, "page_refresh_confirmation_buttons": false, "website_privacy_policy_url": "https://plugin.wphowto.tv/privacy-policy/" });
+    // Only if the website is in accessed directly (not in iframe)
+    if (window.self === window.top) {
+        cookieconsent.run({ "notice_banner_type": "headline", "consent_type": "simple", "palette": "dark", "language": "en", "page_load_consent_levels": ["strictly-necessary"], "notice_banner_reject_button_hide": false, "preferences_center_close_button_hide": false, "page_refresh_confirmation_buttons": false, "website_privacy_policy_url": "https://plugin.wphowto.tv/privacy-policy/" });
+    }
 });
